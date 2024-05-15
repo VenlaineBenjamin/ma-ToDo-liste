a915aabbbf35e99f4b2155fd4bcadb0ad2c4cef0
benjamin <benjaminverlaine@gmail.com>
2024-05-15 16:26:00 +0200

    finish projet v1.0.0

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ const ul = document.querySelector("ul");
 const form = document.querySelector("form");
 const input = document.querySelector("form > input");
 
-const todos = [
-    {
-        text: "je suis une todo",
-        done: false,
-    },
-    {
-        text: "faire du javascript",
-        done: true,
-    },
-];
-
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const value = input.value;
@@ -22,9 +11,26 @@ form.addEventListener("submit", (event) => {
     addTodo(value);
 });
 
+const todos = [
+    {
+        text: "je suis une todo",
+        done: false,
+        editMode: false,
+    },
+    {
+        text: "faire du JavaScript",
+        done: true,
+        editMode: false,
+    },
+];
+
 const displayTodo = () => {
     const todosNode = todos.map((todo, index) => {
-        return createTodoElement(todo, index);
+        if (todo.editMode) {
+            return createTodoEditElement(todo, index);
+        } else {
+            return createTodoElement(todo, index);
+        }
     });
     ul.innerHTML = "";
     ul.append(...todosNode);
@@ -34,13 +40,43 @@ const createTodoElement = (todo, index) => {
     const li = document.createElement("li");
     const buttonDelete = document.createElement("button");
     buttonDelete.innerHTML = "Supprimer";
+    const buttonEdit = document.createElement("button");
+    buttonEdit.innerHTML = "Edit";
     buttonDelete.addEventListener("click", (event) => {
+        event.stopPropagation();
         deleteTodo(index);
+    });
+    buttonEdit.addEventListener("click", (event) => {
+        event.stopPropagation();
+        toggleEditMode(index);
     });
     li.innerHTML = `
     <span class="todo ${todo.done ? "done" : ""}"></span>
     <p>${todo.text}</p>`;
-    li.appendChild(buttonDelete);
+    li.addEventListener("click", (event) => {
+        toggleTodo(index);
+    });
+    li.append(buttonEdit, buttonDelete);
+    return li;
+};
+
+const createTodoEditElement = (todo, index) => {
+    const li = document.createElement("li");
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = todo.text;
+    const buttonSave = document.createElement("button");
+    buttonSave.innerHTML = "Save";
+    const buttonCancel = document.createElement("button");
+    buttonCancel.innerHTML = "Cancel";
+    buttonCancel.addEventListener("click", (event) => {
+        event.stopPropagation();
+        toggleEditMode(index);
+    });
+    buttonSave.addEventListener("click", (event) => {
+        editTodo(index, input);
+    });
+    li.append(input, buttonCancel, buttonSave);
     return li;
 };
 
@@ -54,6 +90,23 @@ const addTodo = (text) => {
 
 const deleteTodo = (index) => {
     todos.splice(index, 1);
+    displayTodo();
+};
+
+const toggleTodo = (index) => {
+    todos[index].done = !todos[index].done;
+    displayTodo();
+};
+
+const toggleEditMode = (index) => {
+    todos[index].editMode = !todos[index].editMode;
+    displayTodo();
+};
+
+const editTodo = (index, input) => {
+    const value = input.value;
+    todos[index].text = value;
+    todos[index].editMode = false;
     displayTodo();
 };
 
